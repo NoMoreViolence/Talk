2a96e811411bf9623ba31208ea2109174efd6ad7
NoMoreViolence <ljh86029926@gmail.com>
2017-09-22 12:02:24 +0900

    Main Page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+// express 모듈
+const express = require('express');
+const router = express.Router();
+// 몽고 디비
+const Client = require('mongodb').MongoClient;
 
+// 유저 정보 디비, 블로그 포스트 디비
+const User = require(__dirname + '/User.js');
+const ChatData = require(__dirname + '/ChatData.js');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
