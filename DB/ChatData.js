364537c316181a7ae462b7b34ff7f06ed6207758
NoMoreViolence <ljh86029926@gmail.com>
2017-09-22 13:44:12 +0900

    DB settings

--- DB/ChatData.js
+++ DB/ChatData.js
@@ -3,4 +3,4 @@ const userSchema = new mongoose.Schema({
     Chatting: String, // 채팅 기록
     Number: String // 채팅 날린 사람 정보
 });
-module.exports = mongoose.model('Messages', userSchema);
+module.exports = mongoose.model('Message', userSchema);
