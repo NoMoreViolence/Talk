0d4d7425e3d74d725e35f638dbcc4c73a323e649
NoMoreViolence <ljh86029926@gmail.com>
2017-09-22 09:48:45 +0900

    User and Message DB settings

--- DB/ChatData.js
+++ DB/ChatData.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
-    Chatting: [String], // 채팅 기록
-    Number: Number
+    Chatting: String, // 채팅 기록
+    Number: String // 채팅 날린 사람 정보
 });
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('Messages ', userSchema);
--- DB/User.js
+++ DB/User.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
-    username: { type: String, unique: true, trim: true }, // 중복 X
+    username: { type: String, unique: true, trim: true }, // 중복 X, 공백 제거
+    id: { type: String, unique: true, trim: true }, // 중복 X, 공백 제거
     password: { type: String, trim: true }, // 공백제거
     email: { type: String, unique: true, trim: true }, // 중복 X, 공백제거
     age: Number, // 나이
-    PeopleNumber: String, // 사람 고유 번호
-    ChatLOG: [String] // 채팅 번호 기록
+    PeopleNumber: Number, // 사람 고유 번호
 });
 module.exports = mongoose.model('User', userSchema);
